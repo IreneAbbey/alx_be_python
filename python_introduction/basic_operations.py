0f0c5da675d8e6cbed3a973585b3fac193236fe4
Irene Abbey <abbeyirene09@gmail.com>
2024-12-11 17:33:36 +0000

    Python Basics

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,14 @@
+number1 = 10
+number2 = 5
+
+# SUM OF NUMBERS
+Sum = number1 + number2
+print(f"Sum of {number1} and {number2} is {Sum}")
+
+# DIFFERENCE OF NUMBERS
+Difference = number2 - number1
+print(f"Difference of {number2} and {number1} is {Difference}")
+
+# PRODUCT OF NUMBERS
+Product = number1 * number2
+print(f"Product of {number1} and {number2} is {Product}")
