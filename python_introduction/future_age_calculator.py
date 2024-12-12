be72468749b284ecf1f0c6ab4769a432eb7f2b7e
Irene Abbey <abbeyirene09@gmail.com>
2024-12-12 11:26:41 +0000

    Python basic tasks

--- python_introduction/future_age_calculator.py
+++ python_introduction/future_age_calculator.py
@@ -1,7 +1,15 @@
 # Asking for users age
-Current_age = int(input("How are old you?"))
+def Current_age():
+    while True:
+        try:
+            # Prompt the user for their current age
+            current_age = int(input("How old are you? "))
+            return current_age
+        except ValueError:
+            print("Invalid input. Please enter a valid integer.")
 
+present_age = Current_age(30)
 
 # Calculating their future age
-age = Current_age + 27
+age = present_age + 27
 print(f"In 2050, you will be {age} years old")
