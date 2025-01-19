dbfbd485329e54de4ede972b0cd9669fecdbfa32
Irene Djagbley Abbey <abbeyirene09@gmail.com>
2025-01-19 21:55:40 +0000

    Update robust_division_calculator.py

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -6,5 +6,4 @@ def safe_divide(numerator, denominator):
         return "Error: Cannot divide by zero."
     except ValueError:
         return "Error: Please enter numeric values only."
-    finally:
-        print("Safe division operation completed.")
+
